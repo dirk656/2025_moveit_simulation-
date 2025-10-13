6bacb1d392c0e382677a264a3c9bc2c0ac460e96
feng <1608749191@qq.com>
2025-10-13 09:28:14 +0800

    franka机械臂测试版本

---
+++ build/moveit_resources_panda_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/feng/franka_simulation/src/panda_description
+# Build directory: /home/feng/franka_simulation/src/build/moveit_resources_panda_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
