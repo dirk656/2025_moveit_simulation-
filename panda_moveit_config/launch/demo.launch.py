6267b203ffe6d7e74c967ada1fbfe94e6ac9adaa
feng <1608749191@qq.com>
2025-10-19 21:57:12 +0800

    还没有解决联合调试的问题

--- panda_moveit_config/launch/demo.launch.py
+++ panda_moveit_config/launch/demo.launch.py
@@ -1,18 +1,23 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('panda_moveit_config')
-
     gazebo_launch = os.path.join(pkg_path, 'launch', 'gazebo.launch.py')
     move_group_launch = os.path.join(pkg_path, 'launch', 'move_group.launch.py')
     rviz_launch = os.path.join(pkg_path, 'launch', 'moveit_rviz.launch.py')
 
     return LaunchDescription([
         IncludeLaunchDescription(PythonLaunchDescriptionSource(gazebo_launch)),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(move_group_launch)),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(rviz_launch)),
+        TimerAction(
+            period=10.0,
+            actions=[IncludeLaunchDescription(PythonLaunchDescriptionSource(move_group_launch))]
+        ),
+        TimerAction(
+            period=12.0,
+            actions=[IncludeLaunchDescription(PythonLaunchDescriptionSource(rviz_launch))]
+        ),
     ])
